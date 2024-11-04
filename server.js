708d8f9fb80e78a0344bbe64ea90b71878b81c0e
nelsonmaia <nellson.maia@gmail.com>
2024-11-04 21:54:31 +0000

    using node-app-attest library instead

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-import express from 'express';
-import { join } from 'path';
-import morgan from 'morgan';
-import helmet from 'helmet';
-import crypto from 'crypto';
-import jwt from 'jsonwebtoken';
-import { verifyAttestation } from 'node-app-attest';
+const express = require("express");
+const { join } = require("path");
+const morgan = require("morgan");
+const helmet = require("helmet");
+const crypto = require("crypto");
+const jwt = require('jsonwebtoken');
+const { verifyAttestation } = await import('node-app-attest');
 
 const app = express();
 
@@ -13,7 +13,6 @@ app.use(helmet());
 app.use(express.json());
 app.use(express.static(join(__dirname, "public")));
 
-// Load the private key for JWT signing
 const privateKey = process.env.PRIVATE_KEY;
 const auth0PrivateKey = process.env.AUTH0_PRIVATE_KEY;
 
@@ -43,7 +42,7 @@ app.post("/verify-attestation", async (req, res) => {
         });
 
         console.log("Attestation result:", result);
-
+        
         return res.status(200).json({
             message: "Attestation verified successfully",
             publicKey: result.publicKey,
@@ -55,6 +54,7 @@ app.post("/verify-attestation", async (req, res) => {
     }
 });
 
+// Challenge endpoint for app attestation
 app.get("/get-challenge", (req, res) => {
     const challenge = crypto.randomBytes(32).toString('base64');
     res.status(200).json({ challenge });
@@ -68,4 +68,4 @@ process.on("SIGINT", function() {
     process.exit();
 });
 
-export default app;
+module.exports = app;
