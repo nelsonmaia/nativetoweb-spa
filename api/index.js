3cf1003eddf8c6ce6d8f1ba22d9cf0a74e26c3f5
nelsonmaia <nellson.maia@gmail.com>
2024-11-04 22:39:46 +0000

    using require again

--- api/index.js
+++ api/index.js
@@ -1,5 +1,4 @@
-import app from '../server.js';
-
+const app = require('../server');
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
--- server.js
+++ server.js
@@ -1,9 +1,10 @@
-import express from 'express';
-import { join } from 'path';
-import morgan from 'morgan';
-import helmet from 'helmet';
-import crypto from 'crypto';
-import { verifyAttestation } from 'node-app-attest';
+const express = require("express");
+const { join } = require("path");
+const morgan = require("morgan");
+const helmet = require("helmet");
+const crypto = require("crypto");
+const jwt = require('jsonwebtoken');
+
 
 const app = express();
 
@@ -12,7 +13,6 @@ app.use(helmet());
 app.use(express.json());
 app.use(express.static(join(__dirname, "public")));
 
-// Load the private key for JWT signing
 const privateKey = process.env.PRIVATE_KEY;
 const auth0PrivateKey = process.env.AUTH0_PRIVATE_KEY;
 
@@ -32,6 +32,8 @@ app.post("/verify-attestation", async (req, res) => {
         console.log("Received keyId:", keyId);
         console.log("Received attestation (base64):", attestation);
 
+        const { verifyAttestation } = await import('node-app-attest');
+
         const result = verifyAttestation({
             attestation: Buffer.from(attestation, 'base64'),
             challenge: challenge,
@@ -42,7 +44,7 @@ app.post("/verify-attestation", async (req, res) => {
         });
 
         console.log("Attestation result:", result);
-
+        
         return res.status(200).json({
             message: "Attestation verified successfully",
             publicKey: result.publicKey,
@@ -54,6 +56,7 @@ app.post("/verify-attestation", async (req, res) => {
     }
 });
 
+// Challenge endpoint for app attestation
 app.get("/get-challenge", (req, res) => {
     const challenge = crypto.randomBytes(32).toString('base64');
     res.status(200).json({ challenge });
@@ -67,4 +70,4 @@ process.on("SIGINT", function() {
     process.exit();
 });
 
-export default app;
+module.exports = app;
